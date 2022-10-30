316f7e6909e380471359a273a7741dd3a789e18b
Shurik12 <alex-chester@mail.ru>
2022-10-30 18:28:37 +0300

    added logger, async

--- yandex_music_api/AsyncLoader.py
+++ yandex_music_api/AsyncLoader.py
@@ -2,7 +2,9 @@ import os
 import json
 from time import time
 from yandex_music.track import track
-from tqdm import tqdm
+from tqdm.asyncio import tqdm
+import asyncio
+import logging
 
 class Loader:
     def __init__(self, client, logger):
@@ -10,12 +12,12 @@ class Loader:
         self.logger = logger
 
     def decorator_time_execution(function):
-        def time_execution(*args, **kwargs):
+        async def time_execution(*args, **kwargs):
             start_execution = time()
-            function(*args, **kwargs)
+            await function(*args, **kwargs)
             end_execution = time()
             execution_time = 'Execution time {:.2f} seconds'.format(end_execution - start_execution)
-            print(execution_time)
+            print (execution_time)
         return time_execution
 
     def get_playlists(self):
@@ -24,11 +26,18 @@ class Loader:
             print(play_list.title, play_list.track_count)
             traks = play_list.tracks
 
-    @decorator_time_execution
-    def download_like_tracks(self):
+    async def make_coro(self, future):
+        try:
+            return await future
+        except asyncio.CancelledError:
+            return await future
 
-        tracks_ids = list(map(lambda track: track["id"], self.client.users_likes_tracks().tracks))
-        tracks = self.client.tracks(tracks_ids)
+    @decorator_time_execution
+    async def download_like_tracks(self):
+
+        tracks = await self.client.users_likes_tracks()
+        tracks_ids = list(map(lambda track: track["id"], tracks.tracks))
+        tracks = await self.client.tracks(tracks_ids)
         tracks_for_download = []
         loaded_tracks = set(os.listdir("output/tracks/"))
 
@@ -39,16 +48,30 @@ class Loader:
                 tracks_for_download.append(track)
         print ("Got %d tracks" % len(tracks_for_download))
 
-        for i in tqdm(range(len(tracks_for_download)), "Loading..."):
+        loop = asyncio.get_running_loop() # get current event loop
+        tasks = [] # create download tasks for await
+
+        # in this function we create tasks in event loop for downloading songs
+        async for i in tqdm(range(len(tracks_for_download)), "Loading..."):
             track = tracks_for_download[i]
             duration = '{:.2f}'.format(track.duration_ms/60000)
             artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
             if track.lyrics_available:
-                supplement = track.get_supplement()
-                #  + "\n\n" + "Duration: " + str(duration)
+                supplement = await track.get_supplement()
                 file_lyrics = 'output/lyrics/' + artist + " - " + track.title.replace("/", "\\") + ".txt"
                 if supplement.lyrics and supplement.lyrics.full_lyrics:
                     with open(file_lyrics, "w") as f_w:
                         f_w.write(supplement.lyrics.full_lyrics)
             file_track = 'output/tracks/' + artist + " - " + track.title.replace("/", "\\") + ".mp3"
-            track.download(file_track, 'mp3', 192)
+            tasks.append(loop.create_task(self.wrapper(track.download_async, file_track, 'mp3', 192)))
+        
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+        self.logger.info(f'Result: {result}')
+
+    async def wrapper(self, func, file_name, extension, bitrate):
+        try:
+            await func(file_name, extension, bitrate)
+            return 1
+        except Exception as e:
+            self.logger.warning(f'{file_name}')
+            return -1
--- yandex_music_api/main.py
+++ yandex_music_api/main.py
@@ -1,16 +1,44 @@
 import os
+import logging
 from yandex_music import Client
+from yandex_music import ClientAsync
 from Loader import Loader
+from AsyncLoader import Loader as LoaderAsync
+import asyncio
+import time
 
 # from yandex_music import ClientAsync
 
+def exception_handler(loop, context):
+	print(context)
+
+def create_logger():
+	logger = logging.getLogger('Logger')
+	logger.setLevel(logging.INFO) 
+	handler = logging.FileHandler(f'logs/{time.strftime("%Y-%m-%d")}')
+	handler.setFormatter(logging.Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
+	logger.addHandler(handler)
+	return logger
+
+async def main_async():
+
+	logger = create_logger()
+	logger.info(f'New launch asynchron')
+	loop = asyncio.get_running_loop()
+	client_async = await ClientAsync(os.environ["YANDEX_MUSIC_TOKEN"]).init()
+	alex = LoaderAsync(client_async, logger)
+	await alex.download_like_tracks()
+	logger.info(f'Launch finished asynchron')
+
 def main():
 
+	logger = create_logger()
+	logger.info(f'New launch synchron')
 	client = Client(os.environ["YANDEX_MUSIC_TOKEN"]).init()
-	
-	alex = Loader(client)
-
+	alex = Loader(client, logger)
 	alex.download_like_tracks()
+	logger.info(f'Launch finished synchron')
 
 if __name__ == '__main__':
 	main()
+	# asyncio.run(main_async())
