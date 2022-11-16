b0f21dd962836bb74f7ffc9e06add497f16ddac8
Shurik12 <alex-chester@mail.ru>
2022-11-16 15:30:58 +0300

    v.2.0.0

--- yandex_music_api/Loader.py
+++ yandex_music_api/Loader.py
@@ -3,6 +3,7 @@ import json
 from time import time
 from yandex_music.track import track
 from tqdm import tqdm
+import logging
 
 class Loader:
     def __init__(self, client, logger):
@@ -29,6 +30,7 @@ class Loader:
 
         tracks_ids = list(map(lambda track: track["id"], self.client.users_likes_tracks().tracks))
         tracks = self.client.tracks(tracks_ids)
+        bitrates = []
         tracks_for_download = []
         loaded_tracks = set(os.listdir("output/tracks/"))
 
@@ -41,14 +43,38 @@ class Loader:
 
         for i in tqdm(range(len(tracks_for_download)), "Loading..."):
             track = tracks_for_download[i]
-            duration = '{:.2f}'.format(track.duration_ms/60000)
-            artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
-            if track.lyrics_available:
-                supplement = track.get_supplement()
-                #  + "\n\n" + "Duration: " + str(duration)
-                file_lyrics = 'output/lyrics/' + artist + " - " + track.title.replace("/", "\\") + ".txt"
-                if supplement.lyrics and supplement.lyrics.full_lyrics:
-                    with open(file_lyrics, "w") as f_w:
-                        f_w.write(supplement.lyrics.full_lyrics)
-            file_track = 'output/tracks/' + artist + " - " + track.title.replace("/", "\\") + ".mp3"
-            track.download(file_track, 'mp3', 192)
+            if track.available:
+                download_info = track.get_download_info()
+                bitrates = [x["bitrate_in_kbps"] for x in download_info]
+                duration = '{:.2f}'.format(track.duration_ms/60000)
+                artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
+                file_name = artist + " - " + track.title.replace("/", "\\")
+                # print(len(file_name))
+                if len(file_name) > 100:
+                    file_name = file_name[:100]
+                if track.lyrics_available:
+                    supplement = track.get_supplement()
+                    #  + "\n\n" + "Duration: " + str(duration)                
+                    file_lyrics = 'output/lyrics/' + file_name + ".txt"
+                    if supplement.lyrics and supplement.lyrics.full_lyrics:
+                        with open(file_lyrics, "w") as f_w:
+                            f_w.write(supplement.lyrics.full_lyrics)
+                            self.logger.info(f'Lyrics: {file_lyrics}')
+                    else:
+                        self.logger.info(f'No lyrics: {file_lyrics}')
+                else:
+                    self.logger.info(f'No lyrics: {file_name}')
+                file_track = 'output/tracks/' + file_name + ".mp3"
+                if 320 in bitrates:
+                    track.download(file_track, 'mp3', 320)
+                    self.logger.info(f'{file_track}\t320')
+                elif 192 in bitrates:
+                    track.download(file_track, 'mp3', 192)
+                    self.logger.info(f'{file_track}\t192')
+                else:
+                    track.download(file_track, 'mp3', 128)
+                    self.logger.info(f'{file_track}\t128')
+            else:
+                artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
+                file_name = artist + " - " + track.title.replace("/", "\\")
+                self.logger.warning(f'Track unavaliable: {file_name}')
