4f342631d3c1c7c1197f2f2dc000e3ad5df6f448
Shurik12 <alex-chester@mail.ru>
2024-01-01 19:23:20 +0300

    update yandex music loader

--- yandex_music_api/Loader.py
+++ yandex_music_api/Loader.py
@@ -9,6 +9,7 @@ class Loader:
     def __init__(self, client, logger):
         self.client = client
         self.logger = logger
+        self.output = "/home/alex/yandex_disk/Music"
 
     def decorator_time_execution(function):
         def time_execution(*args, **kwargs):
@@ -36,19 +37,18 @@ class Loader:
                 # duration = '{:.2f}'.format(track.duration_ms/60000)            
                 if len(file_name) > 100:
                     file_name = file_name[:100]
-                if track.lyrics_available:
+                if track.lyrics_available: 
                     supplement = track.get_supplement()
-                    #  + "\n\n" + "Duration: " + str(duration)                
-                    file_lyrics = lyrics_dir + file_name + ".txt"
+                    #  + "\n\n" + "Duration: " + str(duration)
                     if supplement.lyrics and supplement.lyrics.full_lyrics:
-                        with open(file_lyrics, "w") as f_w:
-                            f_w.write(supplement.lyrics.full_lyrics)
+                        with open(lyrics_dir+ "/" + file_name + ".txt", "w") as file_lyrics:
+                            file_lyrics.write(supplement.lyrics.full_lyrics)
                             self.logger.info(f'Lyrics: {file_lyrics}')
                     else:
-                        self.logger.info(f'No lyrics: {file_lyrics}')
+                        self.logger.warning(f'No lyrics: {lyrics_dir}/{file_name}.txt')
                 else:
                     self.logger.info(f'No lyrics: {file_name}')
-                file_track = tracks_dir + file_name + ".mp3"
+                file_track = tracks_dir + "/" + file_name + ".mp3"
                 # check bitrates
                 if 320 in bitrates:
                     track.download(file_track, 'mp3', 320)
@@ -65,50 +65,72 @@ class Loader:
                 self.logger.warning(f'Track unavaliable: {file_name}')
 
     def download_playlist(self, playlist):
-        print (playlist.title)
-        playlist_dir = f'output/playlists/{playlist.title}/'
-        tracks_dir = f'output/playlists/{playlist.title}/tracks/'
-        lyrics_dir = f'output/playlists/{playlist.title}/lyrics/'
+        self.logger.info(f'Playlist to download: {playlist.title}')
+        playlist_dir = f'{self.output}/{playlist.title}'
+        tracks_dir = f'{playlist_dir}/tracks'
+        lyrics_dir = f'{playlist_dir}/lyrics'
 
-        if playlist.title not in set(os.listdir("output/playlists")): 
+        # Create playlist directory if not exists
+        if playlist.title not in set(os.listdir(self.output)): 
             os.mkdir(playlist_dir)
             os.mkdir(tracks_dir)
             os.mkdir(lyrics_dir)
-        tracks_ids = [track["id"] for track in playlist.fetch_tracks()]
-        tracks = self.client.tracks(tracks_ids)
-        tracks_for_download = []
-        loaded_tracks = set(os.listdir(tracks_dir))
 
+        # Get already loaded tracks (already on yandex disk and file system)
+        tracks_ids = [track["id"] for track in playlist.fetch_tracks()]
+        if (len(tracks_ids) == 0):
+            self.logger.warning(f'No tracks in playlist: {playlist.title}')
+            return 0
+
+        tracks = self.client.tracks(tracks_ids)
+        self.logger.info(f'All tracks in playlist {playlist.title}: {len(tracks)}')
+        loaded_tracks = set(os.listdir(tracks_dir))
+        self.logger.info(f'Already loaded tracks in playlist {playlist.title}: {len(loaded_tracks)}')
+
+        # Download new tracks
+        tracks_for_download = []
         for track in tqdm(tracks, "Get tracks for download"):
             artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
             name = artist + " - " + track.title.replace("/", "\\")
             name = len(name) > 100 and name[:100] + ".mp3" or name + ".mp3" 
             if name not in loaded_tracks:		
                 tracks_for_download.append(track)
-        print ("Got %d tracks" % len(tracks_for_download))
+        self.logger.info(f'Tracks for download: {len(tracks_for_download)}')
         self.download_tracks(tracks_for_download, lyrics_dir, tracks_dir)
+
         return 0
 
     @decorator_time_execution
     def download_playlists(self):
         playlists = self.client.users_playlists_list()
-        if "playlists" not in set(os.listdir("output/")): 
-            os.mkdir('output/playlists')
         return [self.download_playlist(playlist) for playlist in playlists]
 
     @decorator_time_execution
     def download_like_tracks(self):
-        tracks_dir = "output/tracks/"
-        lyrics_dir = "output/lyrics/"
+
+        playlist_dir = f'{self.output}/Like'
+        tracks_dir = f'{playlist_dir}/tracks'
+        lyrics_dir = f'{playlist_dir}/lyrics'
+
+        # Create Like playlist directory if not exists
+        if playlist.title not in set(os.listdir(self.output)): 
+            os.mkdir(playlist_dir)
+            os.mkdir(tracks_dir)
+            os.mkdir(lyrics_dir)
+
+        # Get already loaded tracks (already on yandex disk and file system)
         tracks_ids = list(map(lambda track: track["id"], self.client.users_likes_tracks().tracks))
         tracks = self.client.tracks(tracks_ids)
-        tracks_for_download = []
+        self.logger.info(f'All tracks in playlist {playlist.title}: {len(tracks)}')
         loaded_tracks = set(os.listdir(tracks_dir))
+        self.logger.info(f'Already loaded tracks in playlist {playlist.title}: {len(loaded_tracks)}')
 
+        # Download new tracks
+        tracks_for_download = []
         for track in tqdm(tracks, "Get tracks for download"):
             artist = ", ".join(track.artists_name()).strip().replace("/", "\\")
             name = artist + " - " + track.title.replace("/", "\\") + ".mp3"
             if name not in loaded_tracks:		
                 tracks_for_download.append(track)
-        print ("Got %d tracks" % len(tracks_for_download))
+        self.logger.info(f'Tracks for download: {len(tracks_for_download)}')
         self.download_tracks(tracks_for_download, lyrics_dir, tracks_dir)
